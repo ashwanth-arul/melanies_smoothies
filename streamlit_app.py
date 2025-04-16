69003ac3b520010ad429936b7d0413b7d8e30078
Ashwanth <94758465+ashwanth-arul@users.noreply.github.com>
2025-04-16 11:55:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,4 +41,10 @@ if ingredients_list:
 
     #st.write(my_insert_stmt)
     #st.stop()
+
+#New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
   
